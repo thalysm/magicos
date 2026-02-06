67b49344b8983f2d3b38917290591091091d2002
Thalys M <thalyscorreia@hotmail.com>
2026-02-06 11:43:09 -0300

    refact: corrigindo erro de local incorreto

--- profiledef.sh
+++ profiledef.sh
@@ -12,6 +12,8 @@ bootmodes=('bios.syslinux'
            'uefi.systemd-boot')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
+
+
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
